23b5a7f6a4870082813d6e1897f395fc36813c13
Francesca-Barletta <wallebar@gmail.com>
2024-03-06 16:23:21 +0100

    add console screen

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log('fizzbuzz')
+
 /*Consegna:
 Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
 per i multipli di 3 stampi “Fizz” al posto del numero e
@@ -18,17 +18,58 @@ Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda ch
 inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
 
-//stampare in console i numeri da 1 a 100
-  //dichiarare una variabile uguale a 100
+
 //per i multipli di 3 stampare in console "fizz" al posto del numero
   //con operatore modulo determinare se un numero è multiplo di 3 quindi il numero deve essere divisibile per 3 con resto 0
 //per i multipli di 5 stampare la parola "buzz" al posto del numero
   //con operatore modulo determinare se un numero è multiplo di 5 quindi il numero deve essere divisibile per 5 con resto 0  
-// se un nuuero è sia multiplo di 3 che di 5 dobbiamo stampare "fizzbuzz" al posto del numero
+// se un numero è sia multiplo di 3 che di 5 dobbiamo stampare "fizzbuzz" al posto del numero
 
+
+  //dichiaro una variabile n uguale a 100
 let n = 100;
-
+ //dichiaro una variabile i uguale a 0 che deve ripetersi tanto quanto vale n
+ // in questo modo avrò i numeri da 0 a 99
 for(let i = 0; i < n; i++){
+ // dichiaro una variabile num che è uguale a i + 1 come risultato avrò i numeri da 1 a 100
     let num = i + 1;
+//devo stampare i numeri da 1 a 100 ma 
+    //SE un numero è multiplo di 3 devo stampare fizz invece di num
+    //SE un numero è multiplo di 5 devo stampare buzz invece di num
+    //SE un numero è sia multiplo di 3 che di 5 devo stampare fizzbuzz
+    
+    //per i multipli di 3 stampare in console "fizz" al posto del numero
+        //con operatore modulo determinare se un numero è multiplo di 3
+        
+
+    let multiploTre = num % 3;
+    //quindi il numero deve essere divisibile per 3 con resto 0
+   
+  
+   
+    //per i multipli di 5 stampare la parola "buzz" al posto del numero
+        //con operatore modulo determinare se un numero è multiplo di 5
+             
+    let multiploCinque = num % 5;
+     //quindi il numero deve essere divisibile per 5 con resto 0  
+   
+    
+    
+// se il numero è uguale a un multiplo di 3 e uguale a un multiplo di 5 stampo fizzbuzz
+ if (multiploCinque === 0 &&
+     multiploTre === 0){
+        console.log('fizzbuzz')
+    }
+    // se il numero è uguale a un multiplo di 5 stampo buzz
+ else if(multiploCinque === 0){
+        console.log('buzz')
+ }
+    //se invece  il numero è un multiplo di 3 stampo fizz
+  else if(multiploTre === 0){
+        console.log('fizz')
+  }
+    //altrimenti il numero non è ne multiplo di 3 ne di 5 stampo numero
+  else{
     console.log(num)
+  }
 }
